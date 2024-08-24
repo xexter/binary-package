528d664f68ced9306951b5687bd822743f3f7d91
xexter <madhavpolawar2012@gmail.com>
2024-08-24 23:16:29 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) {
+            return mid; // Target found
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search the right half
+        } else {
+            right = mid - 1; // Search the left half
+        }
+    }
+    return -1; // Target not found
+}
+
+module.exports = binarySearch;
